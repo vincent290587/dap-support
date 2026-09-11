4dc16ef1bb2fbd549384dc9b5bc19662918c848b
Vincent Golle <vincent.golle@gmail.com>
2026-09-11 09:25:25 +0200

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd DapLink
+cd DAPLink
 
-git reset --hard
+#git reset --hard
+
+python tools/progen_compile.py -t make_gcc_arm k20dx_bl
 
 python tools/progen_compile.py -t make_gcc_arm k20dx_if
 
